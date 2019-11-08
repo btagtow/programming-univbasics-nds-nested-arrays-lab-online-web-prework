1a0901b8a256b872f1484fb1ae4ba40a0fdcc9ab
Ben Tagtow <bentagtow@gmail.com>
2019-11-08 17:41:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,6 +34,7 @@ def array_literal_matrix
     "Michael Pemulis"
     ]
     array_literal.sort 
+    array_literal
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
